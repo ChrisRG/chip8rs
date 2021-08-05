741963f5cfd9a1e6cd89081885b69872286c8fa4
Chris Geekie <chris.geekie@gmail.com>
2021-08-05 13:29:38 +0200

    Refactored run loop to chip8 struct

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,21 +1,33 @@
-use minifb::Key;
+use core::time;
 use std::{fs::File, io::Read};
+use std::{
+    thread,
+    time::{Duration, Instant},
+};
+
+use rodio::{OutputStream, Sink};
+
+use minifb::{Key, KeyRepeat, Window, WindowOptions};
 
 use crate::bus::Bus;
 use crate::cpu::Cpu;
 use crate::disassembler::Disassembler;
 
+const SCREEN_WIDTH: usize = 640;
+const SCREEN_HEIGHT: usize = 320;
+
 pub struct Chip8 {
     bus: Bus,
     cpu: Cpu,
     rom: Vec<u8>,
+    rom_filepath: String,
 }
 
 impl Chip8 {
     pub fn new(rom_file: String) -> Chip8 {
-        println!("Starting CHIP-8 emulator...");
         let mut rom_buffer = Vec::<u8>::new();
-        let mut file = File::open(rom_file).expect("File not found");
+        let mut file = File::open(&rom_file).expect("File not found");
+
         if let Ok(bytes_read) = file.read_to_end(&mut rom_buffer) {
             println!("{} bytes loaded", bytes_read);
         } else {
@@ -26,38 +38,119 @@ impl Chip8 {
             bus: Bus::new(),
             cpu: Cpu::new(&rom_buffer),
             rom: rom_buffer,
+            rom_filepath: rom_file,
         }
     }
 
-    pub fn disassemble(&self, rom_name: String) {
+    pub fn run(&mut self) {
+        let mut window = Window::new(
+            "CHIP8RS",
+            SCREEN_WIDTH,
+            SCREEN_HEIGHT,
+            WindowOptions::default(),
+        )
+        .unwrap_or_else(|e| {
+            panic!("Window creation failed: {:?}", e);
+        });
+
+        // Sound
+        let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+        let sink = Sink::try_new(&stream_handle).unwrap();
+        let source = rodio::source::SineWave::new(400);
+        sink.append(source);
+        sink.pause();
+
+        let mut buffer: Vec<u32> = vec![0; SCREEN_WIDTH * SCREEN_HEIGHT];
+        let mut last_key_update_time = Instant::now();
+        let sleep_count = time::Duration::from_millis(1);
+
+        while window.is_open() && !window.is_key_down(Key::Escape) {
+            let key = self.check_key(window.get_keys_pressed(KeyRepeat::Yes));
+            if key.is_some() || Instant::now() - last_key_update_time >= Duration::from_millis(200)
+            {
+                last_key_update_time = Instant::now();
+                self.set_key_pressed(key);
+            }
+
+            self.execute_cycle();
+
+            if self.should_redraw() {
+                buffer = self.update_display(&buffer);
+                window
+                    .update_with_buffer(&buffer, SCREEN_WIDTH, SCREEN_HEIGHT)
+                    .unwrap();
+            }
+
+            if self.should_beep() {
+                sink.play();
+            } else {
+                sink.pause();
+            }
+
+            thread::sleep(sleep_count / 2);
+        }
+    }
+
+    fn check_key(&self, keys_pressed: Option<Vec<Key>>) -> Option<Key> {
+        match keys_pressed {
+            Some(keys) => {
+                if !keys.is_empty() {
+                    Some(keys[0])
+                } else {
+                    None
+                }
+            }
+            None => None,
+        }
+    }
+
+    fn update_display(&self, in_buffer: &Vec<u32>) -> Vec<u32> {
+        let mut buffer = in_buffer.clone();
+        let chip8_buffer = self.get_frame_buffer();
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let index = self.get_frame_index(x / 10, y / 10);
+                let pixel = chip8_buffer[index];
+
+                let color = if pixel == 1 { 0x00ff00 } else { 0x0 };
+                let offset = y * SCREEN_WIDTH + x;
+                buffer[offset] = color;
+            }
+        }
+        buffer
+    }
+
+    pub fn disassemble(&self) {
+        // TODO: pass in cpu ram to Disassembler rather than needing a rom field
         let disassembler = Disassembler::new(&self.rom);
-        let result = disassembler.run(rom_name);
+        let result = disassembler.run(self.rom_filepath.clone());
         match result {
             Ok(_) => println!("ROM disassembly written to file"),
             Err(_) => println!("Error in disassembling ROM"),
         };
     }
 
-    pub fn execute_cycle(&mut self) {
+    fn execute_cycle(&mut self) {
         self.cpu.execute_cycle(&mut self.bus);
     }
 
-    pub fn get_frame_buffer(&self) -> &[u8] {
+    fn get_frame_buffer(&self) -> &[u8] {
         self.bus.display.get_frame_buffer()
     }
 
-    pub fn get_frame_index(&self, x: usize, y: usize) -> usize {
+    fn get_frame_index(&self, x: usize, y: usize) -> usize {
         self.bus.display.get_index(x, y)
     }
 
-    pub fn set_key_pressed(&mut self, key: Option<Key>) {
+    fn set_key_pressed(&mut self, key: Option<Key>) {
         self.bus.set_key_pressed(key);
     }
 
-    pub fn should_redraw(&self) -> bool {
+    fn should_redraw(&self) -> bool {
         self.cpu.should_redraw()
     }
-    pub fn should_beep(&self) -> bool {
+
+    fn should_beep(&self) -> bool {
         self.cpu.should_beep()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,107 +5,23 @@ mod disassembler;
 mod display;
 mod font;
 mod ram;
-use core::time;
-use std::{
-    thread,
-    time::{Duration, Instant},
-};
+use crate::chip8::Chip8;
 
 #[macro_use]
 extern crate clap;
 use clap::App;
 
-use rodio::{OutputStream, Sink};
-
-use minifb::{Key, KeyRepeat, Window, WindowOptions};
-
-use crate::chip8::Chip8;
-
-const SCREEN_WIDTH: usize = 640;
-const SCREEN_HEIGHT: usize = 320;
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
     let rom_file = matches.value_of("INPUT").unwrap().to_string();
-    let mut chip8 = Chip8::new(rom_file.clone());
+    println!("[Starting CHIP-8 emulator]");
+    let mut chip8 = Chip8::new(rom_file);
 
     if matches.is_present("disassemble") {
-        chip8.disassemble(rom_file);
+        chip8.disassemble();
     }
 
-    let mut window = Window::new(
-        "CHIP8RS",
-        SCREEN_WIDTH,
-        SCREEN_HEIGHT,
-        WindowOptions::default(),
-    )
-    .unwrap_or_else(|e| {
-        panic!("Window creation failed: {:?}", e);
-    });
-
-    // Sound
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&stream_handle).unwrap();
-    let source = rodio::source::SineWave::new(400);
-    sink.append(source);
-    sink.pause();
-
-    let mut buffer: Vec<u32> = vec![0; SCREEN_WIDTH * SCREEN_HEIGHT];
-    let mut last_key_update_time = Instant::now();
-    let sleep_count = time::Duration::from_millis(1);
-
-    while window.is_open() && !window.is_key_down(Key::Escape) {
-        let key = check_key(window.get_keys_pressed(KeyRepeat::Yes));
-        if key.is_some() || Instant::now() - last_key_update_time >= Duration::from_millis(200) {
-            last_key_update_time = Instant::now();
-            chip8.set_key_pressed(key);
-        }
-
-        chip8.execute_cycle();
-
-        if chip8.should_redraw() {
-            buffer = update_display(&chip8, &buffer);
-            window
-                .update_with_buffer(&buffer, SCREEN_WIDTH, SCREEN_HEIGHT)
-                .unwrap();
-        }
-
-        if chip8.should_beep() {
-            sink.play();
-        } else {
-            sink.pause();
-        }
-
-        thread::sleep(sleep_count / 2);
-    }
-}
-
-fn check_key(keys_pressed: Option<Vec<Key>>) -> Option<Key> {
-    match keys_pressed {
-        Some(keys) => {
-            if !keys.is_empty() {
-                Some(keys[0])
-            } else {
-                None
-            }
-        }
-        None => None,
-    }
-}
-
-fn update_display(chip8: &Chip8, in_buffer: &Vec<u32>) -> Vec<u32> {
-    let mut buffer = in_buffer.clone();
-    let chip8_buffer = chip8.get_frame_buffer();
-    for y in 0..SCREEN_HEIGHT {
-        for x in 0..SCREEN_WIDTH {
-            let index = chip8.get_frame_index(x / 10, y / 10);
-            let pixel = chip8_buffer[index];
-
-            let color = if pixel == 1 { 0x00ff00 } else { 0x0 };
-            let offset = y * SCREEN_WIDTH + x;
-            buffer[offset] = color;
-        }
-    }
-    buffer
+    chip8.run();
 }
